f6b50fde52477ae2c15e608c0719d3ed7a18da01
Domício Medeiros <domicioam@gmail.com>
2019-01-03 10:14:32 -0200

    Adicionando input-group ao formulário de pagamentos.

--- client/src/components/NotasFiscais/NotaFiscalForm/PagamentoForm/PagamentoForm.js
+++ client/src/components/NotasFiscais/NotaFiscalForm/PagamentoForm/PagamentoForm.js
@@ -20,22 +20,32 @@ class PagamentoForm extends Component {
   render() {
     return (
       <div className="row">
-      <fieldset className="form-group col-lg-2">
-        <label>Parcelas:</label>
-        <input type="text" className="form-control form-control-sm" />
-      </fieldset>
-      <fieldset className="form-group col-lg-2">
-        <label>Valor Parcela:</label>
-        <input type="text" className="form-control form-control-sm" />
-      </fieldset>
-      <fieldset className="form-group col-lg-2">
-        <label>Forma:</label>
-        <input type="text" className="form-control form-control-sm" />
-      </fieldset>
-      <fieldset className="form-group col-lg-auto">
-        <button id="btnGravarPagamento" className="btn btn-success btn-sm float-right">Gravar Pagamento</button>
-      </fieldset>
-    </div>
+        <fieldset className="form-group col-lg-2">
+          <label>Parcelas:</label>
+          <input type="number" className="form-control form-control-sm" />
+        </fieldset>
+        <fieldset className="form-group col-lg-2">
+          <label>Valor Parcela:</label>
+          <div className="input-group input-group-sm">
+            <div className="input-group-prepend">
+              <span className="input-group-text">R$</span>
+            </div>
+            <input type="text" className="form-control form-control-sm" />
+          </div>
+        </fieldset>
+        <fieldset className="form-group col-lg-2">
+          <label>Forma:</label>
+          <select className="form-control form-control-sm">
+              <option value="dinheiro">Dinheiro</option>
+              <option value="débito">Cartão de Débito</option>
+              <option value="crédito">Cartão de Crédito</option>
+              
+            </select>
+        </fieldset>
+        <fieldset className="form-group col-lg-auto">
+          <button id="btnGravarPagamento" className="btn btn-success btn-sm float-right">Gravar Pagamento</button>
+        </fieldset>
+      </div>
     );
   }
 }
