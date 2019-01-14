44b3fdcf305436f49eafecaa71459783713e034d
Domício Medeiros <domicioam@gmail.com>
2019-01-14 09:20:27 -0200

    Alterando layout de form de modal para page.

--- client/src/components/NotasFiscais/NotasFiscais.js
+++ client/src/components/NotasFiscais/NotasFiscais.js
@@ -3,18 +3,25 @@ import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import './NotasFiscais.css';
-import NotaFiscalModal from '../NotaFiscalModal/NotaFiscalModal';
+import NotaFiscalForm from '../NotaFiscalForm/NotaFiscalForm';
 
 export default class NotasFiscais extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { notas: [] };
+    this.state = {
+      displayForm: true,
+      notas: []
+    };
   }
 
   async componentDidMount() {
     const res = await axios.get('/api/notasfiscais');
     this.setState({ notas: res.data });
+  }
+
+  toogleDisplayForm = () => {
+    this.setState({ displayForm: !this.state.displayForm });
   }
 
   renderContent() {
@@ -40,36 +47,59 @@ export default class NotasFiscais extends Component {
     return notas;
   }
 
+  renderList() {
+    if (!this.state.displayForm) {
+
+      return (
+        <React.Fragment>
+          <header className="row">
+            <div className="col-lg-12">
+              <button onClick={this.toogleDisplayForm} className="btn btn-success float-right">Nova NF-e</button>
+              <button onClick={this.toogleDisplayForm} className="btn btn-success float-right">Nova NFC-e</button>
+            </div>
+          </header>
+          <div className="table-responsive">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>Modelo</th>
+                  <th>Número</th>
+                  <th>Data / Hora Emissão</th>
+                  <th>Data / Hora Autorização</th>
+                  <th>Destinatário</th>
+                  <th>UF</th>
+                  <th>Valor</th>
+                  <th>Status</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.renderContent()}
+              </tbody>
+            </table>
+          </div>
+        </React.Fragment>
+      );
+    } else {
+      return null;
+    }
+  }
+
+  renderForm() {
+    if (this.state.displayForm) {
+      return (
+        <NotaFiscalForm toogleDisplayForm={this.toogleDisplayForm} />
+      );
+    } else {
+      return null;
+    }
+  }
+
   render() {
     return (
       <section className="NotasFiscais">
-        <header className="row">
-          <div className="col-lg-12">
-            <button className="btn btn-success float-right" data-toggle="modal" data-target="#exampleModal">Nova NF-e</button>
-            <button className="btn btn-success float-right" data-toggle="modal" data-target="#exampleModal">Nova NFC-e</button>
-          </div>
-        </header>
-        <div className="table-responsive">
-          <table className="table table-striped">
-            <thead>
-              <tr>
-                <th>Modelo</th>
-                <th>Número</th>
-                <th>Data / Hora Emissão</th>
-                <th>Data / Hora Autorização</th>
-                <th>Destinatário</th>
-                <th>UF</th>
-                <th>Valor</th>
-                <th>Status</th>
-                <th>Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.renderContent()}
-            </tbody>
-          </table>
-        </div>
-        <NotaFiscalModal />
+        {this.renderList()}
+        {this.renderForm()}
       </section>
     );
   }
